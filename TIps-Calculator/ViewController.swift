1b24a4c58dd45daeee370a775a780c37c3b55c76
Alexander Shpin <@lexhpin@gmail.com>
2022-09-02 05:35:29 -0500

    Initial Commit

---
+++ TIps-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TIps-Calculator
+//
+//  Created by Alexander Shpin on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
